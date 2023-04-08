8ea28e1aaf421cbb6dbb38d4365c9342bae98913
arnygaard <120501873+arnygaard@users.noreply.github.com>
2023-04-07 22:28:56 -0400

    Add files via upload
    ros package

---
+++ Part02 folder/src/nav.py
@@ -0,0 +1,349 @@
+import rospy
+import time
+from geometry_msgs.msg import Twist
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import sys
+from queue import PriorityQueue
+import cv2 as cv
+from matplotlib import transforms
+
+fig, ax = plt.subplots()
+weight = 200
+
+def moveset(Xi,Yi,Thetai,RPM):
+    UL = RPM[0]
+    UR = RPM[1]
+    t = 0
+    r = 0.038
+    L = 0.354
+    dt = 0.1
+    Xn=Xi
+    Yn=Yi
+    Thetan = 3.14 * Thetai / 180
+    D=0
+    while t<1:
+        t = t + dt
+        t = t + dt
+        Xs = Xn
+        Ys = Yn
+        Xn += 0.5*r * (UL + UR) * math.cos(Thetan) * dt * 200
+        Yn += 0.5*r * (UL + UR) * math.sin(Thetan) * dt * 200
+        Thetan += (r / L) * (UR - UL) * dt
+        D=D + math.sqrt(math.pow((0.5*r * (UL + UR) * math.cos(Thetan) *
+    dt),2)+math.pow((0.5*r * (UL + UR) * math.sin(Thetan) * dt),2))
+    Thetan = 180 * (Thetan) / 3.14
+    return Xn, Yn, Thetan, D, UL, UR
+
+def calc_vel(Xi,Yi,Thetai,UL,UR,X,Y):
+    t = 0
+    r = 0.038
+    L = 0.354
+    dt = 0.1
+    Xn=Xi
+    Yn=Yi
+    Thetan = 3.14 * Thetai / 180
+    while t<1 and Xn != X or Yn != Y:
+        t = t + dt
+        Xs = Xn
+        Ys = Yn
+        Xn += 0.5*r * (UL + UR) * math.cos(Thetan) * dt * weight
+        Yn += 0.5*r * (UL + UR) * math.sin(Thetan) * dt * weight
+        Xd = 0.5*r * (UL + UR) * math.cos(Thetan) * weight
+        Yd = 0.5*r * (UL + UR) * math.sin(Thetan) * weight
+
+        Thetan += (r / L) * (UR - UL) * dt
+        v = np.sqrt((Xn-Xs)**2+(Yn-Ys)**2)
+        s = (np.sqrt((Xd)**2+(Yd)**2))
+        rz = (r/L)*(UR - UL)
+    return s, rz
+
+
+
+
+#calculate cost to come for each node
+def CalcCost(child, parent):
+    xc = child[0]
+    yc = child[1]
+    xp = parent[0]
+    yp = parent[1]
+    lx = np.abs(xp-xc)
+    ly =np.abs(yp-yc)
+    cost = round(np.sqrt(lx**2 + ly**2),1)
+    return cost
+
+#calculate cost to go for each node
+def CalcCostGo(node, goal):
+    x_dist = goal[0] - node[0]
+    y_dist = goal[1] - node[1]
+    dist = np.sqrt(x_dist**2 + y_dist**2)
+    return dist
+
+#create empty map
+map_empty = np.zeros([600, 200, 3], dtype=np.uint8)
+map_copy = map_empty.copy()
+
+#buffer input
+buffer = 20
+
+#Creating all obstacles on map
+#creating square shape dimensions and assigning pixel values to map
+x_square_start = 120 #150
+x_square_end = 165
+y_square_start = 75
+y_square_end = 199
+for i in range(x_square_start, x_square_end):
+    for j in range(y_square_start, y_square_end):
+        map_empty[i,j,:] = [239,76,76]
+
+#plotting buffer zone
+for i in range(x_square_start-buffer, x_square_end+buffer):
+    for j in range(y_square_start-buffer, y_square_end):
+            if map_empty[i,j,0] != 239:
+                map_empty[i,j,:] = [200,16,16]
+
+
+#second square
+x_square_start = 225 #250
+x_square_end = 265
+y_square_start = 0
+y_square_end = 125
+for i in range(x_square_start, x_square_end):
+    for j in range(y_square_start, y_square_end):
+        map_empty[i,j,:] = [239,76,76]
+
+#plotting buffer zone
+for i in range(x_square_start-buffer, x_square_end+buffer):
+    for j in range(y_square_start, y_square_end+buffer):
+            if map_empty[i,j,0] != 239:
+                map_empty[i,j,:] = [200,16,16]
+
+#creating half circle dimensions and assigning pixel values to map
+x_hc = 400
+hc_r = 50
+y_hc = 110
+for i in range(x_hc - hc_r, x_hc + hc_r):
+    for j in range(y_hc - hc_r, y_hc + hc_r):
+        if (i - x_hc) **2 + (j - y_hc)**2 <= hc_r**2:  # and j > y_hc:
+            map_empty[i,j,:] = [239,76,76]
+
+#plotting buffer zone
+for i in range(x_hc - hc_r - buffer, x_hc + hc_r + buffer):
+    for j in range(y_hc - hc_r - buffer, y_hc + hc_r + buffer):
+        if (i - x_hc) **2 + (j - y_hc)**2 <= (hc_r+buffer)**2 and map_empty[i,j,0] != 239: #and j > y_hc - 5 
+            map_empty[i,j,:] = [200,16,16]
+
+for i in range(0,600):
+    for j in range(0,200):
+        if i < 25:
+            map_empty[i,j,:] = [239,76,76]
+        if i > 594 - 10:
+            map_empty[i,j,:] = [239,76,76]
+        if j < 6 + 10:
+            map_empty[i,j,:] = [239,76,76]
+        if j > 193 - 10:
+            map_empty[i,j,:] = [239,76,76]
+
+print('Enter goal node x:')
+gx = int(input())
+print('Enter goal node y:')
+gy = int(input())
+
+print('Goal Node:',gx, gy)
+print('RPMs: 5, 10')
+print('clearance: 200 mm')
+
+# #assign values and dimensions
+Xs = [50, 100, 0, 110,  0, 0]
+goal = [gx, gy]
+
+dim = (600,200)
+
+actions=[[5,5], [10,10],[5,0],[0,5],[5,10],[10,5]]
+
+v_thresh = 1
+#creating visit matrix
+vdimx = int(600/v_thresh)
+vdimy = int(200/v_thresh)
+vdim = (vdimx,vdimy)
+V = np.zeros(vdim)
+
+#checking for goalpoint in obstacle
+if map_empty[gx,gy,0] != 0:
+    print("goal lies in obstacle")
+    sys.exit()
+
+#creating cost to come and total cost matricies
+cost_map = np.zeros(vdim)
+for i in range(0, 600):
+    for j in range(0,200):
+        if map_empty[i,j,0] == 0:
+            cost_map[int(i/v_thresh),int(j/v_thresh)] = 1E9
+        else:
+            cost_map[int(i/v_thresh),int(j/v_thresh)] = -1
+
+total_cost_map = np.zeros(vdim)
+for i in range(0, 600):
+    for j in range(0,200):
+        if map_empty[i,j,0] == 0:
+            total_cost_map[int(i/v_thresh),int(j/v_thresh)] = 1E9
+        else:
+            total_cost_map[int(i/v_thresh),int(j/v_thresh)] = -1
+
+#initialize variables
+goal_state = 1
+OpenList = PriorityQueue()
+open_list = []
+ClosedList = []
+closed_list = []
+cost_map[Xs[0],Xs[1]] = 0
+goal_thresh = 10
+d = 2
+parents = {}
+rpm = {}
+thetas = {}
+c = 0
+testing_node = []
+#start timer
+start_time = time.time()
+
+
+#begin A* algorithm
+print("Beginning search")
+OpenList.put((0,Xs))
+while OpenList and goal_state != 0:
+
+    #start exploring node
+    Node_State_i = OpenList.get()[1]
+
+    V[[int(round(Node_State_i[0]/v_thresh))],[int(round(Node_State_i[1]/v_thresh))]] = 1
+
+    #if node is within goal threshold, end loop
+    if Node_State_i[0] > goal[0]-goal_thresh and Node_State_i[1] > goal[1]-goal_thresh and Node_State_i[0] < goal[0]+goal_thresh and Node_State_i[1] < goal[1]+goal_thresh:
+        print('SUCCESS')
+        goal[0] = Node_State_i[0]
+        goal[1] = Node_State_i[1]
+        cost2come = cost_map[int(round(Node_State_i[0])),int(round(Node_State_i[1]))]
+        cost_map[int(round(Node_State_i[0])),int(round(Node_State_i[1]))] = cost2come
+        break
+
+    for action in actions:
+        k = moveset(Node_State_i[0],Node_State_i[1],Node_State_i[2],action)
+        #print(k[0],k[1])
+        i = int(round(k[0]))
+        j = int(round(k[1]))
+        if j < 200 and j > 0 and i < 600 and i > 0:
+            if map_empty[i,j,0] == 0:
+                testing_node.append(k)
+
+    #loop through each possible move
+    for item in testing_node:
+        #if node hasn't been visited, calculate the costs and mark as visited
+        if V[[int(round(item[0]/v_thresh))],[int(round(item[1]/v_thresh))]] == 0:
+            if item is not OpenList:
+                V[[int(round(item[0]/v_thresh))],[int(round(item[1]/v_thresh))]] == 1
+                cost2come = item[3] + cost_map[int(round(Node_State_i[0]/v_thresh)),int(round(Node_State_i[1]/v_thresh))]
+                cost_map[int(round(item[0]/v_thresh)),int(round(item[1]/v_thresh))] = cost2come
+                cost2go = CalcCostGo(item,goal)
+                cost = cost2come + cost2go
+                total_cost_map[int(round(item[0]/v_thresh)),int(round(item[1]/v_thresh))] = cost
+
+                parents.setdefault((Node_State_i[0],Node_State_i[1]), [])
+                parents[Node_State_i[0],Node_State_i[1]].append([item[0],item[1]])
+                
+                rpm[item[0],item[1]] = (item[4],item[5])
+                thetas[item[0],item[1]] = item[2]
+
+                OpenList.put((cost,item))
+        #if node has been visited, check to see if new total cost is less than the current total cost
+        else:
+            cost2come = item[3] + cost_map[int(round(Node_State_i[0]/v_thresh)),int(round(Node_State_i[1]/v_thresh))]
+            cost2go = CalcCostGo(item,goal)
+            cost = cost2come + cost2go
+            #if new total cost is greater, update with lower value
+            if total_cost_map[int(round(item[0]/v_thresh)),int(round(item[1]/v_thresh))] > cost:
+                total_cost_map[int(round(item[0]/v_thresh)),int(round(item[1]/v_thresh))] = cost
+
+                parents.setdefault((Node_State_i[0],Node_State_i[1]), [])
+                parents[Node_State_i[0],Node_State_i[1]].append([item[0],item[1]])
+
+                rpm[item[0],item[1]] = (item[4],item[5])
+                thetas[item[0],item[1]] = item[2]
+
+    testing_node = []
+    c = 0
+
+#end timer and print
+print("A* search took %s seconds" % (time.time() - start_time))
+
+#Backtracking algorithm
+key_list=list(parents.keys())
+rk_list = list(rpm.keys())
+rv_list = list(rpm.values())
+val_list=list(parents.values())
+
+print("Generating Path...")
+path = []
+spin = []
+theta = []
+path.append(goal)
+node = goal
+start = [Xs[0], Xs[1]]
+while node != start:
+    for list in val_list:
+        if node in list:
+            ind = val_list.index(list)
+            path.append(key_list[ind])
+            node = [key_list[ind][0], key_list[ind][1]]
+path.reverse()
+
+#finding corresponding RPMs
+rpm[(Xs[0],Xs[1])] = (10,10)
+node = []
+while node != goal:
+    for node in path:
+        if node == goal:
+            break
+        spin.append(rpm[node])
+spin.append((0,0))
+spin.append((0,0))
+
+#finding corresponding thetas
+thetas[(Xs[0],Xs[1])] = 0.0
+node = []
+while node != goal:
+    for node in path:
+        if node == goal:
+            break
+        theta.append(thetas[node])
+theta.append(0.0)
+theta.append(0.0)
+
+def nav():
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    rospy.init_node('nav', anonymous=False)
+    
+    rate = rospy.Rate(1)
+    vel_msg = Twist()
+    Xn = 50
+    Yn = 100
+    s = 0.10*260
+    #open loop for publishing to cmd_vel
+    while Xn != goal[0] and Yn != goal[1]:
+        for i in range(0,len(path)-1):
+            v, rz = calc_vel(path[i][0],path[i][1],theta[i],spin[i+1][0],spin[i+1][1],path[i+1][0],path[i+1][1])
+
+            if i >= 10 and abs(rz) > 0.4:
+                v = s
+
+            vel_msg.linear.x = v/250 #conversion to gazebo needed to scale down both angular and linear velocity to work
+            vel_msg.angular.z = rz/3.34 #conversion to gazebo needed to scale down both angular and linear velocity to work
+            pub.publish(vel_msg)
+            time.sleep(1.9) 
+
+if __name__ == '__main__':
+    try:
+        nav()
+    except rospy.ROSInterruptException:
+        pass
